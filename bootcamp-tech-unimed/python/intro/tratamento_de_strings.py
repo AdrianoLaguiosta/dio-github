5a1f85062130eb84c795a043300cd679387f86f0
AdrianoLaguiosta <adrianol.dev@gmail.com>
2022-09-27 16:00:29 -0300

    Strings

---
+++ bootcamp-tech-unimed/python/intro/tratamento_de_strings.py
@@ -0,0 +1,32 @@
+# 1
+# nome = "AdrIanO"
+#
+# print(nome.upper())
+# print(nome.lower())
+# print(nome.title())
+#
+# texto = "  Olá mundo!   "
+#
+# print(texto)
+# print(texto.strip())
+# print(texto.lstrip())
+# print(texto.rstrip())
+#
+# nome = "Python"
+#
+# print('####' + nome + "####")
+# print(nome.center(14, '#'))
+#
+# print("P-y-t-h-o-n")
+# print('-'.join(nome))
+
+nome = "Adriano Laguiosta"
+
+print(nome[0])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:16:2])
+print(nome[:])
+print(nome[:: - 1])
+
